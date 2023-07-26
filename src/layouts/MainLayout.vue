ddbfef8712cbd83e7b029e023c09a54a476d5c00
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 21:48:13 +0900

    필요없는 파일 삭제 및 기본 폴더 구조 세팅

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import MainHeader from '@/layouts/MainHeader.vue'
+import NavigationBar from '@/layouts/GlobalNavigationBar.vue'
+</script>
+
+<template>
+  <header>
+    <MainHeader>
+      <template #navigation>
+        <NavigationBar />
+      </template>
+    </MainHeader>
+  </header>
+
+  <main>
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </main>
+</template>
+
+<style scoped></style>
