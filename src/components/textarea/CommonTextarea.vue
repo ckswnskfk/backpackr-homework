33789cccb81f8c1b7d785d55151ec0ba7848c33f
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 23:00:57 +0900

    Feat: CommonTextarea 컴포넌트 추가
    기본적인 textarea와 남은 글자수를 보여주는 컴포넌트

---
+++ src/components/textarea/CommonTextarea.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-area__item">
+    <textarea
+      :value="inputMessage"
+      @input="handleInputChange"
+      placeholder="입력해주세요"
+      :maxlength="maxInputLength"
+      :disabled="disabled"
+      class="text-area__item--textarea"
+      cols="30"
+      wrap="hard"
+    ></textarea>
+    <em class="text-area__item--em">{{ countOfLeftCharacter }}</em>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, watchEffect } from 'vue'
+
+interface Props {
+  maxInputLength: number
+  defaultMessage?: string
+  disabled: boolean
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits(['input-changed'])
+
+const inputMessage = ref(props.defaultMessage || '')
+const countOfLeftCharacter = computed(() => props.maxInputLength - inputMessage.value.length)
+
+const hasValidInput = computed(() => {
+  return checkUserWrite()
+})
+
+const isDefaultMessageExists = computed(
+  () => props.defaultMessage && props.defaultMessage.trim() !== ''
+)
+
+const checkUserWrite = (): boolean => {
+  return (
+    inputMessage.value.length > 0 &&
+    (!isDefaultMessageExists.value || props.defaultMessage !== inputMessage.value)
+  )
+}
+const handleInputChange = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  inputMessage.value = target.value
+}
+
+watchEffect(() => {
+  emit('input-changed', hasValidInput.value)
+})
+</script>
