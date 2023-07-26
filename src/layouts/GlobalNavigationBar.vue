370696cc8f949a63c3d49e321b38c3fc57e826a1
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 22:04:45 +0900

    네비게이션 바 구성  및 기본 views 페이지 라우터 등록

---
+++ src/layouts/GlobalNavigationBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <ul>
+      <li><RouterLink to="/">Card List</RouterLink></li>
+      <li><RouterLink to="/input">Input Form</RouterLink></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
