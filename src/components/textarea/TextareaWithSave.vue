3dfdb4d0e327534f4b8e33925f3ea1c60d67ee96
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 23:07:21 +0900

    Feat: TextareaWithSave 컴포넌트, TextAreaForm 추가
    저장 버튼을 포함한 TextareaWithSave, textarea UI 들을 보여줄 페이지 TextAreaForm 추가

---
+++ src/components/textarea/TextareaWithSave.vue
@@ -0,0 +1,30 @@
+<template>
+  <CommonTextarea
+    :max-input-length="maxInputLength"
+    :default-message="defaultMessage"
+    :disabled="disabled"
+    @input-changed="onTextWritten"
+  >
+  </CommonTextarea>
+  <div class="text-area__btn">
+    <button class="text-area__btn--save" :class="{ disabled: isButtonDisabled }">저장</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import CommonTextarea from './CommonTextarea.vue'
+
+interface Props {
+  maxInputLength: number
+  defaultMessage?: string
+  disabled: boolean
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<Props>()
+
+const isUserWritten = ref(false)
+const isButtonDisabled = computed(() => !isUserWritten.value)
+const onTextWritten = (bool: boolean) => (isUserWritten.value = bool)
+</script>
--- src/views/TextAreaForm.vue
+++ src/views/TextAreaForm.vue
@@ -1,5 +1,34 @@
 <template>
-  <div></div>
+  <section>
+    <h2>Text Area Form</h2>
+    <div class="text-area">
+      <TextareaWithSave
+        :max-input-length="MAX_INPUT_LENGTH"
+        default-message=""
+        :disabled="false"
+      ></TextareaWithSave>
+    </div>
+    <div class="text-area">
+      <CommonTextarea
+        :max-input-length="MAX_INPUT_LENGTH"
+        default-message="기본메시지"
+        :disabled="true"
+      >
+      </CommonTextarea>
+    </div>
+    <div class="text-area">
+      <CommonTextarea
+        :max-input-length="MAX_INPUT_LENGTH"
+        default-message="기본메시지"
+        :disabled="false"
+      >
+      </CommonTextarea>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TextareaWithSave from '@/components/textarea/TextareaWithSave.vue'
+import CommonTextarea from '@/components/textarea/CommonTextarea.vue'
+import { MAX_INPUT_LENGTH } from '@/constants'
+</script>
