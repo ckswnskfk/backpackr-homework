83d3a4066a4bc4f2799fbff5d8e8b09e811fcd25
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 23:09:11 +0900

    Feat: card 기본 마크업 작성

--- src/views/CardList.vue
+++ src/views/CardList.vue
@@ -1,5 +1,79 @@
 <template>
-  <div></div>
+  <section>
+    <h2>Card List</h2>
+    <ul class="card-list">
+      <li class="card">
+        <a href="#" class="card__image"><img src="@/assets/idus.jpg" alt="상품 사진" /></a>
+        <div class="card__desc">
+          <div class="card__desc--label">card label</div>
+          <div class="card__desc--title">card title</div>
+          <div class="card__desc--hilight">Hilight <span>Cross Out</span></div>
+          <!-- <div class="card__desc--crossout">Cross Out</div> -->
+        </div>
+        <div class="card__rating--container">
+          <div class="card__rating">●●●○○</div>
+          <p class="card__rating--brief-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
+            iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
+            Eaque autem deserunt adipisci quis.
+          </p>
+        </div>
+      </li>
+      <li class="card">
+        <a href="#" class="card__image"><img src="" alt="상품 사진" /></a>
+        <div class="card__desc">
+          <div class="card__desc--label">card label</div>
+          <div class="card__desc--title">card title</div>
+          <div class="card__desc--hilight">Hilight <span>Cross Out</span></div>
+          <!-- <div class="card__desc--crossout">Cross Out</div> -->
+        </div>
+        <div class="card__rating--container">
+          <div class="card__rating">●●●○○</div>
+          <p class="card__rating--brief-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
+            iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
+            Eaque autem deserunt adipisci quis.
+          </p>
+        </div>
+      </li>
+      <li class="card">
+        <a href="#" class="card__image"><img src="" alt="상품 사진" /></a>
+        <div class="card__desc">
+          <div class="card__desc--label">card label</div>
+          <div class="card__desc--title">card title</div>
+          <div class="card__desc--hilight">Hilight <span>Cross Out</span></div>
+          <!-- <div class="card__desc--crossout">Cross Out</div> -->
+        </div>
+        <div class="card__rating--container">
+          <div class="card__rating">●●●○○</div>
+          <p class="card__rating--brief-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
+            iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
+            Eaque autem deserunt adipisci quis.
+          </p>
+        </div>
+      </li>
+      <li class="card card__horizontal">
+        <a href="#" class="card__image"><img src="" alt="상품 사진" /></a>
+        <div>
+          <div class="card__desc">
+            <div class="card__desc--label">card label</div>
+            <div class="card__desc--title">card title</div>
+            <div class="card__desc--hilight">Hilight <span>Cross Out</span></div>
+            <!-- <div class="card__desc--crossout">Cross Out</div> -->
+          </div>
+          <div class="card__rating--container">
+            <div class="card__rating">●●●○○</div>
+            <p class="card__rating--brief-desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
+              iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
+              Eaque autem deserunt adipisci quis.
+            </p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup lang="ts"></script>
