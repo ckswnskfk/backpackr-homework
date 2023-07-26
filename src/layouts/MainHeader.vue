c2c19a3c2c4e6fbc6bb5c8d10ac255a338629ff1
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-26 21:53:56 +0900

    메인 헤더 작성

---
+++ src/layouts/MainHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="/" class="header__logo">
+    <img
+      src="https://image.idus.com/image/files/d6843d7f602840cdbe7b827286f30460_320.jpg"
+      alt="아이디어스 로고"
+    />
+  </a>
+  <h1>idus homework</h1>
+  <slot name="navigation"></slot>
+</template>
+
+<script setup lang="ts"></script>
