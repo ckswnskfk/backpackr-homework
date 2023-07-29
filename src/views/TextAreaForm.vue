cf9c24da680969702a7231121d8a565de481895f
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-29 21:34:34 +0900

    Feat: 웹 접근성 향상을 위한 screen only 작성

--- src/views/TextAreaForm.vue
+++ src/views/TextAreaForm.vue
@@ -1,27 +1,32 @@
 <template>
   <section>
     <h2>Text Area Form</h2>
-    <fieldset class="text-area">
-      <TextareaWithSave
-        :max-input-length="MAX_INPUT_LENGTH"
-        default-message="초기 메시지"
-        :disabled="false"
-        :read-only="false"
-      ></TextareaWithSave>
-    </fieldset>
-    <fieldset class="text-area">
-      <CommonTextarea :max-input-length="MAX_INPUT_LENGTH" :disabled="true" :read-only="false">
-      </CommonTextarea>
-    </fieldset>
-    <fieldset class="text-area">
-      <CommonTextarea
-        :max-input-length="MAX_INPUT_LENGTH"
-        :disabled="false"
-        :read-only="true"
-        default-message="읽기 전용입니다."
-      >
-      </CommonTextarea>
-    </fieldset>
+    <form>
+      <fieldset class="text-area">
+        <legend class="a11yHidden">Editable Text Area</legend>
+        <TextareaWithSave
+          :max-input-length="MAX_INPUT_LENGTH"
+          default-message="초기 메시지"
+          :disabled="false"
+          :read-only="false"
+        ></TextareaWithSave>
+      </fieldset>
+      <fieldset class="text-area">
+        <legend class="a11yHidden">Disabled Text Area</legend>
+        <CommonTextarea :max-input-length="MAX_INPUT_LENGTH" :disabled="true" :read-only="false">
+        </CommonTextarea>
+      </fieldset>
+      <fieldset class="text-area">
+        <legend class="a11yHidden">Read-only Text Area</legend>
+        <CommonTextarea
+          :max-input-length="MAX_INPUT_LENGTH"
+          :disabled="false"
+          :read-only="true"
+          default-message="읽기 전용입니다."
+        >
+        </CommonTextarea>
+      </fieldset>
+    </form>
   </section>
 </template>
 
