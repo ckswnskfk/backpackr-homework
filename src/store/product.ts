b269e343c61951e209a1ec3de04e2d013f3e039b
ckswnskfk <ckswnskfk@hanmail.net>
2023-07-29 13:53:18 +0900

    Refactor: store card에서 product로 더 알맞는 이름으로 변경

--- src/store/product.ts
+++ src/store/product.ts
@@ -1,12 +1,12 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import type { CardItemProps } from '@/types'
+import type { ProductInfoSimple } from '@/types'
 
-export const useCardStore = defineStore('card', () => {
-  const cards = ref<CardItemProps[]>()
-  const getCards = computed(() => cards.value)
-  async function searchCards() {
-    cards.value = [
+export const useProductStore = defineStore('product', () => {
+  const products = ref<ProductInfoSimple[]>()
+  const getProducts = computed(() => products.value)
+  async function searchProducts() {
+    products.value = [
       {
         id: 0,
         img: {
@@ -39,23 +39,23 @@ export const useCardStore = defineStore('card', () => {
           alt: '상품 사진1'
         },
         info: { label: '고요한밤', title: '거룩한밤', hilight: '10,000', cross: '100,000' }
-      },
-      {
-        id: 0,
-        img: {
-          path: '',
-          alt: '상품 사진2'
-        },
-        info: { label: '동해물과', title: '백두산이', hilight: '1,000', cross: '500' },
-        review: {
-          rating: 5,
-          desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
-                  iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
-                  Eaque autem deserunt adipisci quis.`
-        }
       }
+      // {
+      //   id: 0,
+      //   img: {
+      //     path: '',
+      //     alt: '상품 사진2'
+      //   },
+      //   info: { label: '동해물과', title: '백두산이', hilight: '1,000', cross: '500' },
+      //   review: {
+      //     rating: 5,
+      //     desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ut distinctio
+      //             iste repellendus nobis temporibus alias ea odio, sunt, ipsa rem numquam. Quo, ullam.
+      //             Eaque autem deserunt adipisci quis.`
+      //   }
+      // }
     ]
   }
 
-  return { getCards, searchCards }
+  return { getProducts, searchProducts }
 })
